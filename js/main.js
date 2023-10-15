315a5b48dbd66221f9528c4d56ee52e0c9ae0103
Felipe Lentini <99766298+bajo3@users.noreply.github.com>
2023-10-15 13:43:03 -0300

    responsivisando

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('load', function () {
+    let loader = document.querySelector('.loader-container');
     var menu = document.querySelector('.hamburger');
     var menuppal = document.querySelector(".menuppal");
+
+    loader.style.display = 'none';
 
     function toggleMenu(event) {
         menu.classList.toggle('is-active');
@@ -16,10 +19,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     menu.addEventListener('click', toggleMenu, false);
 
-    // Agregar un manejador de eventos a los enlaces del menú
     var menuLinks = document.querySelectorAll('.menuppal ul li a');
     menuLinks.forEach(function (link) {
-        link.addEventListener('click', closeMenu, false);
+        link.addEventListener('click', function (event) {
+            closeMenu(event);
+            var targetId = link.getAttribute('href').substring(1); // Obtén el ID de la sección
+            var targetElement = document.getElementById(targetId); // Encuentra el elemento con el ID
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: 'smooth' }); // Desplaza hacia la sección
+            }
+        }, false);
     });
 });
-    
